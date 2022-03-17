a749b33c47bba7008328b9721e0c5c06c7f8e639
solomonopoku <exquire4us@gmail.com>
2022-03-17 14:03:35 +0000

    Building database

--- app/src/main/java/com/example/deliveroo/database/ImageItem.kt
+++ app/src/main/java/com/example/deliveroo/database/ImageItem.kt
@@ -6,7 +6,11 @@ import androidx.room.PrimaryKey
 
 @Entity (tableName = "unsplash_images")
 data class ImageItem(
-    @PrimaryKey (autoGenerate = true) var itemId: Int,
+    @PrimaryKey (autoGenerate = true) var itemId: Int = 0,
     @ColumnInfo(name = "image_id") val imageName : String,
-    @ColumnInfo(name = "image_urls") val imageUrls : List<String>
+    @ColumnInfo(name = "image_url_raw") val imageUrlRaw : String,
+    @ColumnInfo(name = "image_url_regular") val imageUrlRegular : String,
+    @ColumnInfo(name= "image_url_small") val imageUrlSmall : String,
+    @ColumnInfo(name = "image_url_thumb") val imageUrlThumb: String
+
 )
